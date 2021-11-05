20c16bf96d00e0b446ac018d8c71ef197e7db211
Indrajith C <indrajith.chandran@mozanta.com>
2021-11-05 13:59:42 +0530

    simple flase test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link false', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react test/i);
+  expect(linkElement).toBeInTheDocument();
+});
