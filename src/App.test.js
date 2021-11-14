1f004c1b889a20e9ec0b7d3222462df7a7c6369e
Indrajith C <indrajith.chandran@mozanta.com>
2021-11-14 12:39:02 +0530

    link element by role

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  /** option name is the display text */
+  const linkElement = screen.getByRole("link", { name : /learn react/i });
+  expect(linkElement).toBeInTheDocument(); 
 });
