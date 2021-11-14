919ac228e67aef099f6ac7742dd9a4fbb2b79591
Indrajith C <indrajith.chandran@mozanta.com>
2021-11-14 18:48:56 +0530

    button functionality added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import { useState } from "react";
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
+  /**
+   * This function is used to change button color
+   */
   const clickHandler = () => {
     setButtonColor(newButtonColor);
   }
 
+  /**
+   * This function is used to toggle checkbox
+   */
+  const changeHandler = () => setButtonDisabled(preState => !preState);
+
   return (
     <div>
-      <button style={{ backgroundColor: buttonColor }} onClick={clickHandler} >Change to {newButtonColor}</button>
-      <input type="checkbox" />
+      <button style={{ backgroundColor: buttonColor }}
+        onClick={clickHandler}
+        disabled={buttonDisabled}
+      >Change to {newButtonColor}</button>
+      <input onChange={changeHandler} type="checkbox" />
     </div>)
 };
 
