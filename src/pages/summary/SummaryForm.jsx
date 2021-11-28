59013d9d779bacedb8f4c72b45cf7fc55d698eef
Indrajith C <indrajith.chandran@mozanta.com>
2021-11-28 19:17:06 +0530

    TDD :  basic component

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap"
 
-const SummaryForm = () => <div></div>
+const SummaryForm = () => {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const handleTermsAndConditionToggle = (event) => setTcChecked(event.target.checked);
+
+  return (<Form>
+    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <Form.Check readOnly onClick={handleTermsAndConditionToggle} type="checkbox" label="terms and conditions" />
+    </Form.Group>
+    <Button variant="primary" type="submit" disabled={!tcChecked}>
+      confirm order
+    </Button>
+  </Form>)
+}
 
 export default SummaryForm
