4d3639e0021e6fe304c5052ece48b2077ea79803
Indrajith C <indrajith.chandran@mozanta.com>
2021-11-29 06:48:18 +0530

    label component change

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -11,9 +11,20 @@ const SummaryForm = () => {
    */
   const handleTermsAndConditionToggle = (event) => setTcChecked(event.target.checked);
 
+  const checkboxLabel = (
+    <span>
+      I agree to
+      <span style={{ color: 'blue' }}>
+        Terms and Conditions
+      </span>
+    </span>
+  );
+
   return (<Form>
     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      <Form.Check readOnly onClick={handleTermsAndConditionToggle} type="checkbox" label="terms and conditions" />
+      <Form.Check readOnly onClick={handleTermsAndConditionToggle} type="checkbox" label={
+        checkboxLabel
+      } />
     </Form.Group>
     <Button variant="primary" type="submit" disabled={!tcChecked}>
       confirm order
