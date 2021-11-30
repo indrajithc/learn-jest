065fb4a7d34a96b970fd2a6735d7177a0498ce4c
Indrajith C <indrajith.chandran@mozanta.com>
2021-11-30 10:32:27 +0530

    TDD: component

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,4 +1,35 @@
-export default function Options({ optionType }) {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ScoopOptions from "./ScoopOptions";
+import { Row } from "react-bootstrap";
 
-  return <div />
+const Options = (props) => {
+  const { optionType } = props;
+
+  const [items, setItems] = useState([]);
+
+  // optionType is 'scoops' or 'toppings' 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then(response => setItems(response.data))
+      .catch(error => {
+        // TODO: handle error response
+      });
+  }, [optionType]);
+
+  // TODO: replace 'null' with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOptions : null;
+
+  const optionItems = items.map(item => <ItemComponent
+    optionType
+    name={item.name}
+    magePath={item.imagePath}
+  />);
+
+  return <Row>
+    {optionItems}
+  </Row>
 };
+
+export default Options;
--- src/pages/entry/ScoopOptions.jsx
+++ src/pages/entry/ScoopOptions.jsx
@@ -1,3 +1,15 @@
-export default function ScoopOptions() {
-  return <div />
-}
+import Col from "react-bootstrap/Col"
+
+const ScoopOptions = (props) => {
+  const { name, imagePath } = props;
+  return (<Col xs={12} sm={6} md={4} style={{ textAlgin: 'center' }}>
+    <img
+      style={{ width: '75%' }}
+      className="img"
+      src={`http://localhost:3030/${imagePath}`}
+      alt={`${name} scoop`}
+    />
+  </Col>);
+};
+
+export default ScoopOptions;
