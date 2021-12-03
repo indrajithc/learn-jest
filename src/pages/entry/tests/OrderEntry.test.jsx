22adc47bd88f11e84da1b78a9e119cae9f70a91e
Indrajith C <indrajith.chandran@mozanta.com>
2021-12-03 20:22:09 +0530

    wait for added to finish all api calls

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server"
@@ -15,8 +15,8 @@ test("handler error for scoops and toppings routes", async () => {
 
   render(<OrderEntry />);
 
-  const alerts = await screen.findAllByRole("alert", {
-    name: "An unexpected error ocurred. Please try again later."
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
+    expect(alerts).toHaveLength(2);
   });
-
 });
