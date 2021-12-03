7715066ef576a9ba88c427ec0960230074156f97
Indrajith C <indrajith.chandran@mozanta.com>
2021-12-03 16:23:48 +0530

    mock api response

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -9,5 +9,15 @@ export const handlers = [
         { name: "Vanilla", imagePath: "/images/vanilla.png" }
       ])
     )
+  }),
+  rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+
+    return res(
+      ctx.json([
+        { name: "Cherries", imagePath: "/images/cherries.png" },
+        { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
+        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" }
+      ])
+    );
   })
 ]
